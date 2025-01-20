af7d61cf374d76dc6b4f44d17a8cdae350151d53
Sindhu Hachadad <sindhu.hachadad@sony.com>
2025-01-21 02:36:34 +0530

    scrolling

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -443,7 +443,7 @@ export const Project = () => {
                         </div> : null}
 
                         <div className="mt-10">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-10" >
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-10" id="filteredResults">
             {filteredData.map((member, index) => (
               <div key={index} className="w-full bg-black rounded-lg shadow-lg p-8 flex flex-col justify-between h-full cursor-pointer relative" onClick={() => handleCardClick(member)}>
 
